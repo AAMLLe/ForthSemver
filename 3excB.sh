2820f555d80961972446606e2f5dbe75860c0ea4
AAMLLe <myanh@moneylover.me>
2018-01-12 11:21:57 +0700

    Create 3excB.sh

---
+++ 3excB.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# execute using backticks
+git status
+
+echo `npm run releaseTh`
+
+commit_files() {
+	ls -la
+  # git checkout -b master
+  git add CHANGELOG.md
+  git add package.json
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER [skip ci]"
+  git status
+}
+git rm -r --cached node_modules/
+# git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed [skip ci] "
+commit_files
+git push -fq origin master > /dev/null
+git push --follow-tags origin master
+echo `chandler push`
